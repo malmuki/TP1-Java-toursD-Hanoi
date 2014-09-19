06bdecdd023db0fea6a71006e77ad25c14d3263d
Francois Chantal <geniousmouton@gmail.com>
2014-09-19 10:33:17 -0400

    ajout de la condition de victoire

--- TP1/src/ca/csf/tourHanoi/MainWindowController.java
+++ TP1/src/ca/csf/tourHanoi/MainWindowController.java
@@ -10,31 +10,49 @@ import javafx.scene.control.ChoiceBox;
 import javafx.scene.layout.VBox;
 
 public class MainWindowController {
-	
-	private Tower[] towers = {new Tower(new LinkedListStack()), new Tower(new LinkedListStack()), new Tower(new LinkedListStack())};
-	@FXML Rectangle base;
-	@FXML Button nouvellePartie;
-	@FXML ChoiceBox<?> diskDepart;
-	@FXML VBox tour1vBox;
-	@FXML VBox tour2vBox;
-	@FXML VBox tour3vBox;
-	@FXML Button Tour1;
-	@FXML Button Tour2;
-	@FXML Button Tour3;
-    
-	@FXML public void nouvelle_Partie() {
-		int nbDisk = Integer.parseInt((String) diskDepart.getValue());
-		
+
+	private Tower[] towers = { new Tower(new LinkedListStack()),
+			new Tower(new LinkedListStack()), new Tower(new LinkedListStack()) };
+	@FXML
+	Rectangle base;
+	@FXML
+	Button nouvellePartie;
+	@FXML
+	ChoiceBox<?> diskDepart;
+	@FXML
+	VBox tour1vBox;
+	@FXML
+	VBox tour2vBox;
+	@FXML
+	VBox tour3vBox;
+	@FXML
+	Button Tour1;
+	@FXML
+	Button Tour2;
+	@FXML
+	Button Tour3;
+
+	int nbDisk;
+
+	@FXML
+	public void nouvelle_Partie() {
+		nbDisk = Integer.parseInt((String) diskDepart.getValue());
+
 		tour1vBox.getChildren().clear();
-		
+
 		for (int i = 0; i < nbDisk; i++) {
-			Rectangle rectangle = new Rectangle(20 + i*7, 40);
+			Rectangle rectangle = new Rectangle(20 + i * 7, 40);
 			tour1vBox.getChildren().add(rectangle);
-			towers[0].addDisk(new Disk(20 + i*7));
+			towers[0].addDisk(new Disk(20 + i * 7));
 		}
 	}
-	
-	@FXML public void deplacementDisk() {
-		
+
+	@FXML
+	public void deplacementDisk() {
+		// condition de victoire;
+		if (towers[2].getSize() == nbDisk) {
+
+		}
+
 	}
 }
